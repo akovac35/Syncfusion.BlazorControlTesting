5d62247483e25dcaa8c8fe1d5c6d192a2bc10378
Aleksander Kovač <aleksander.kovac@sberbank.si>
2021-05-01 10:23:22 +0200

    Updated code.

---
+++ Pages/ButtonTesting.razor.cs
@@ -0,0 +1,47 @@
+﻿using com.github.akovac35.Logging;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Syncfusion.BlazorControlTesting.Pages
+{
+    public partial class ButtonTesting: ComponentBase
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        [Inject]
+        ILogger<ButtonTesting> Logger { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        async Task OnClickAsync()
+        {
+            Logger.Here(l => l.Entering(LogLevel.Information));
+            
+            await Task.Delay(500);
+
+            // Here the screen should be updated as per Blazor lifecycle
+
+            await Task.Delay(200);
+
+            Logger.Here(l => l.Exiting(LogLevel.Information));
+        }
+
+        async Task OnClickWithTaskContinuationAsync()
+        {
+            Logger.Here(l => l.Entering(LogLevel.Information));
+
+            await Task.Delay(500).ContinueWith(antecedent => Task.Delay(200).Wait());
+
+            Logger.Here(l => l.Exiting(LogLevel.Information));
+        }
+
+        void OnClick()
+        {
+            Logger.Here(l => l.Entering(LogLevel.Information));
+
+            Task.Delay(500).ContinueWith(antecedent => Task.Delay(200).Wait()).Wait();
+
+            Logger.Here(l => l.Exiting(LogLevel.Information));
+        }
+    }
+}
