b123e81d0e716b01c9d013436d66ef768f54b5ff
Aleksander Kovač <aleksander.kovac@sberbank.si>
2021-05-06 07:42:42 +0200

    Updated code.

---
+++ Pages/SlowPageLoad.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Syncfusion.BlazorControlTesting.Pages
+{
+    public partial class SlowPageLoad
+    {
+        private bool IsInitialized { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if(!IsInitialized)
+            {
+                Task.Delay(3000).Wait();
+                IsInitialized = true;
+            }
+
+            base.OnInitialized();
+        }
+    }
+}
